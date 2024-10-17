ff1cb5bab3d1bd26a96c4c797a3350be41766a08
Best App! <47402822+BestAppGit@users.noreply.github.com>
2024-10-17 10:20:08 -0300

    Update ids.py
    > Atualiza log_pattern
    > Atualiza bots_attemps
    > Ignore logs contendo: "/g/collect" ou "/?gad_source" ou "/assinaturas"

--- ids.py
+++ ids.py
@@ -20,78 +20,55 @@ logging.basicConfig(
 
 # Diretório contendo os arquivos de log
 log_directory = "/www/wwwlogs"
-log_pattern = "*access*"
+log_pattern = "*access_log"
 
-# Expressões regulares para capturar o IP e verificar as requisições
-regex_ip = re.compile(r"^(\d+\.\d+\.\d+\.\d+)")  # IP deve estar na primeira coluna
-regex_404 = re.compile(r' 404 ')
-regex_403 = re.compile(r' 403 ')
-regex_401 = re.compile(r' 401 ')
+# Expressões regulares
+regex_ip = re.compile(r"^(\d+\.\d+\.\d+\.\d+)")
+regex_code_status = re.compile(r' (404|403|401|301) ')
 regex_wp_login = re.compile(r' /wp-login.php')
+regex_url = re.compile(r'//wp-admin')
+regex_bots = re.compile(r'semrush|yandex|mj12bot|babbar.tech|ahrefs.com|DataForSeoBot')
 
-# Conjunto de IPs já suspensos para evitar duplicidade
+# Variáveis de configuração
+max_attempts_code_status = 30
+max_attempts_wp_login = 10
+max_attempts_bots = 3
+max_attempts_url = 5  # Número máximo de tentativas para a regex_url
+time_window_seconds = 300  # 5 minutos de intervalo para contar tentativas
+
+# Conjunto de IPs já suspensos
 suspended_ips = set()
 suspended_ips_lock = threading.Lock()
+bots_ban = set()
 
 # Dicionários para contar tentativas por IP
-attempts_errors = defaultdict(int)  # Inclui 401, 403, 404
+attempts_errors = defaultdict(int)
 attempts_wp_login = defaultdict(int)
+attempts_bots = defaultdict(int)
+attempts_url = defaultdict(int)  # Dicionário para contar tentativas de acesso a /wp-admin
 attempts_lock = threading.Lock()
 
-# Tempo de suspensão em segundos (exemplo: 1 hora)
-suspension_time = 3600  # 1 hora
-ip_suspension_time = {}
-ip_suspension_lock = threading.Lock()
-
 # Função para adicionar IP ao conjunto 'ids_ban' do nftables
-def suspend_ip(ip):
+def suspend_ip(ip, ban_set="ids_ban"):
     with suspended_ips_lock:
         if ip not in suspended_ips:
-            logging.info(f"Suspending IP: {ip}")
+            logging.info(f"Suspending IP: {ip} in {ban_set}")
             try:
                 subprocess.run(
-                    ["sudo", "nft", "add", "element", "ip", "filter", "ids_ban", f'{{ {ip} }}'],
+                    ["sudo", "nft", "add", "element", "ip", "filter", ban_set, f'{{ {ip} }}'],
                     check=True
                 )
                 suspended_ips.add(ip)
-                with ip_suspension_lock:
-                    ip_suspension_time[ip] = time.time()
             except subprocess.CalledProcessError as e:
                 logging.error(f"Erro ao suspender IP {ip}: {e}")
 
-# Função para remover IP do conjunto 'ids_ban' do nftables
-def unsuspend_ip(ip):
-    with suspended_ips_lock:
-        if ip in suspended_ips:
-            logging.info(f"Unsuspending IP: {ip}")
-            try:
-                subprocess.run(
-                    ["sudo", "nft", "delete", "element", "ip", "filter", "ids_ban", f'{{ {ip} }}'],
-                    check=True
-                )
-                suspended_ips.remove(ip)
-                with ip_suspension_lock:
-                    del ip_suspension_time[ip]
-            except subprocess.CalledProcessError as e:
-                logging.error(f"Erro ao unsuspender IP {ip}: {e}")
-
-# Função para seguir um único arquivo de log
-def follow_log(file_path):
-    logging.info(f"Iniciando monitoramento do arquivo: {file_path}")
-    try:
-        with open(file_path, "r") as file:
-            file.seek(0, 2)  # Vai para o final do arquivo
-            while True:
-                line = file.readline()
-                if not line:
-                    time.sleep(0.1)  # Aguarda se não há novas linhas
-                    continue
-                process_line(line)
-    except Exception as e:
-        logging.error(f"Erro ao monitorar o arquivo {file_path}: {e}")
-
 # Função para processar cada linha do log
 def process_line(line):
+    # Ignorar linhas que contêm "/g/collect" ou "/?gad_source"
+    if "/g/collect" in line or "/?gad_source" in line or "/assinaturas" in line:
+        logging.debug(f"Ignorando linha com '/g/collect' ou '/?gad_source' ou '/assinaturas': {line.strip()}")
+        return  # Ignorar esta linha
+
     ip_match = regex_ip.search(line)
     if ip_match:
         ip = ip_match.group(1)
@@ -102,75 +79,85 @@ def process_line(line):
                 attempts_wp_login[ip] += 1
                 count = attempts_wp_login[ip]
             logging.debug(f"IP {ip} tentou /wp-login.php {count} vezes.")
-            if count >= 10:
+            if count >= max_attempts_wp_login:
                 suspend_ip(ip)
         
-        # Contagem de tentativas para 401, 403 ou 404
-        elif regex_404.search(line) or regex_403.search(line) or regex_401.search(line):
+        # Contagem de tentativas para 404, 403, 401 e 301
+        elif regex_code_status.search(line):
             with attempts_lock:
                 attempts_errors[ip] += 1
                 count = attempts_errors[ip]
-            logging.debug(f"IP {ip} gerou {count} erros 401/403/404.")
-            if count >= 25:
+            logging.debug(f"IP {ip} gerou {count} erros 404/403/401/301.")
+            if count >= max_attempts_code_status:
                 suspend_ip(ip)
 
-# Função para resetar as tentativas a cada 5 minutos
+        # Contagem de tentativas para /wp-admin
+        elif regex_url.search(line):
+            with attempts_lock:
+                attempts_url[ip] += 1
+                count = attempts_url[ip]
+            logging.debug(f"IP {ip} tentou acessar URLs deny {count} vezes.")
+            if count >= max_attempts_url:
+                suspend_ip(ip)
+
+        # Contagem de tentativas para bots
+        elif regex_bots.search(line):
+            with attempts_lock:
+                attempts_bots[ip] += 1
+                count = attempts_bots[ip]
+            logging.debug(f"IP {ip} identificado como bot {count} vezes.")
+            if count >= max_attempts_bots:
+                suspend_ip(ip, ban_set="bots_ban")
+
+# Função para resetar as tentativas a cada intervalo definido
 def reset_attempts():
     while True:
-        time.sleep(300)
+        time.sleep(time_window_seconds)
         with attempts_lock:
             attempts_errors.clear()
             attempts_wp_login.clear()
+            attempts_bots.clear()
+            attempts_url.clear()  # Limpa as contagens para /wp-admin
         logging.info("Resetando contagens de tentativas...")
 
-# Função para limpar IPs suspensos após o período de suspensão
-def cleanup_suspended_ips():
-    while True:
-        time.sleep(60)  # Verifica a cada minuto
-        current_time = time.time()
-        ips_to_unsuspend = []
-        with ip_suspension_lock:
-            for ip, suspend_time in list(ip_suspension_time.items()):
-                if current_time - suspend_time > suspension_time:
-                    ips_to_unsuspend.append(ip)
-        
-        for ip in ips_to_unsuspend:
-            unsuspend_ip(ip)
+# Função para seguir um único arquivo de log
+def follow_log(file_path):
+    logging.info(f"Iniciando monitoramento do arquivo: {file_path}")
+    try:
+        with open(file_path, "r") as file:
+            file.seek(0, 2)  # Vai para o final do arquivo
+            while True:
+                line = file.readline()
+                if not line:
+                    time.sleep(0.1)
+                    continue
+                process_line(line)
+    except Exception as e:
+        logging.error(f"Erro ao monitorar o arquivo {file_path}: {e}")
 
 # Função para monitorar todos os arquivos de log existentes e novos
 def monitor_logs():
     monitored_files = set()
 
     with ThreadPoolExecutor(max_workers=100) as executor:
-        # Inicia threads para monitorar os arquivos existentes
         def add_initial_files():
             log_files = glob.glob(os.path.join(log_directory, log_pattern))
             for log_file in log_files:
                 if log_file not in monitored_files:
                     executor.submit(follow_log, log_file)
                     monitored_files.add(log_file)
-        
-        add_initial_files()
 
-        # Inicia threads auxiliares
+        add_initial_files()
         executor.submit(reset_attempts)
-        executor.submit(cleanup_suspended_ips)
-        
-        # Monitora continuamente o diretório para novos arquivos de log
+
         while True:
-            time.sleep(10)  # Intervalo para verificar novos arquivos
+            time.sleep(10)
             log_files = glob.glob(os.path.join(log_directory, log_pattern))
             for log_file in log_files:
                 if log_file not in monitored_files:
                     logging.info(f"Novo arquivo de log detectado: {log_file}")
                     executor.submit(follow_log, log_file)
                     monitored_files.add(log_file)
-            # Opcional: Remover arquivos que foram deletados
-            # arquivos_atualizados = set(glob.glob(os.path.join(log_directory, log_pattern)))
-            # arquivos_para_remover = monitored_files - arquivos_atualizados
-            # for log_file in arquivos_para_remover:
-            #     logging.info(f"Arquivo de log removido: {log_file}")
-            #     monitored_files.remove(log_file)
 
 # Execução do script
 if __name__ == "__main__":
