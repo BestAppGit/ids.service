615c9ba8219983ca6343cce2c6bb8376b457c7d2
Best App! <47402822+BestAppGit@users.noreply.github.com>
2025-02-06 10:57:10 -0300

    Update nftables.sh

--- nftables.sh
+++ nftables.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-#CRIANDO CHAIN INPUT
-nft add chain ip filter INPUT
+#CRIANDO CHAINS
+nft add table ip filter
+nft add chain ip filter INPUT { type filter hook input priority 0 \; policy accept \; }
+nft add chain ip filter FORWARD { type filter hook forward priority 0 \; policy accept \; }
+nft add chain ip filter OUTPUT { type filter hook output priority 0 \; policy accept \; }
+
+nft add table ip6 filter
+nft add chain ip6 filter INPUT { type filter hook input priority 0 \; policy accept \; }
+nft add chain ip6 filter FORWARD { type filter hook forward priority 0 \; policy accept \; }
+nft add chain ip6 filter OUTPUT { type filter hook output priority 0 \; policy accept \; }
 
 #CRIANDO O CONJUNTO WHITELIST, IDS_BAN E BOTS_BAN:
 nft add set ip filter whitelist { type ipv4_addr\; flags interval\; }
-nft add set ip filter ids_ban { type ipv4_addr\; flags timeout\; timeout 24h\; }
-nft add set ip filter bots_ban { type ipv4_addr\; flags timeout\; timeout 48h\; }
+nft add set ip filter ids_ban { type ipv4_addr\; flags interval, timeout\; timeout 24h\; }
+nft add set ip filter bots_ban { type ipv4_addr\; flags interval, timeout\; timeout 48h\; }
 
 #nft add set ip6 filter whitelist { type ipv6_addr\; flags interval\; }
 #nft add set ip6 filter ids_ban { type ipv6_addr\; flags timeout\; timeout 12h\; }
@@ -15,6 +23,7 @@ nft add set ip filter bots_ban { type ipv4_addr\; flags timeout\; timeout 48h\; 
 #CRIANDO REGRA PARA OS CONJUNTOS:
 nft insert rule ip filter INPUT ip saddr @bots_ban drop
 nft insert rule ip filter INPUT ip saddr @ids_ban drop
+nft insert rule ip filter INPUT ip saddr @ntopng_ban drop
 nft insert rule ip filter INPUT ip saddr @whitelist accept
 
 #nft insert rule ip6 filter INPUT ip6 saddr @bots_ban drop
