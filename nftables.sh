3f1c898b9dcfa5c97440176924af18c834219fa4
Mateus Wild <best@bestapp.com.br>
2025-07-01 19:11:53 +0000

    nftables changed

--- nftables.sh
+++ nftables.sh
@@ -16,19 +16,10 @@ nft add set ip filter whitelist { type ipv4_addr\; flags interval\; }
 nft add set ip filter ids_ban { type ipv4_addr\; flags interval\; timeout 24h\; }
 nft add set ip filter bots_ban { type ipv4_addr\; flags interval\; timeout 48h\; }
 
-#nft add set ip6 filter whitelist { type ipv6_addr\; flags interval\; }
-#nft add set ip6 filter ids_ban { type ipv6_addr\; flags timeout\; timeout 12h\; }
-#nft add set ip6 filter bots_ban { type ipv6_addr\; flags timeout\; timeout 24h\; }
-
 #CRIANDO REGRA PARA OS CONJUNTOS:
 nft insert rule ip filter INPUT ip saddr @bots_ban drop
 nft insert rule ip filter INPUT ip saddr @ids_ban drop
-nft insert rule ip filter INPUT ip saddr @ntopng_ban drop
 nft insert rule ip filter INPUT ip saddr @whitelist accept
-
-#nft insert rule ip6 filter INPUT ip6 saddr @bots_ban drop
-#nft insert rule ip6 filter INPUT ip6 saddr @ids_ban drop
-#nft insert rule ip6 filter INPUT ip6 saddr @whitelist accept
 
 #LIMITANDO CONEXÕES ATIVAS
 nft add rule ip filter INPUT tcp dport { 80, 443 } meter conn_meter { ip saddr ct count over 20 } reject
